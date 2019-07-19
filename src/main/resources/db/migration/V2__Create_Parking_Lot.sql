bbfc29c1fb436444a436154618589a84a2666611
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-19 08:19:38 +0800

    Delete V2__Create_Parking_Lot.sql

--- src/main/resources/db/migration/V2__Create_Parking_Lot.sql
+++
@@ -1,8 +0,0 @@
-create table `parking_order`(
-    `id` int auto_increment primary key,
-    `parking_lot_name` varchar(255),
-    `car_number` varchar(255),
-    `create_time` bigint,
-    `end_time` bigint,
-    `order_status` smallint
-)
